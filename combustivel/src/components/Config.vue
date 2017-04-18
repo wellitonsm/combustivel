1442f81a1d2f2b8726cf82276766096c02a8fec8
Welliton Milhomens <welliton.sm@hotmail.com>
2017-04-17 23:07:45 -0300

    meu primeiro commit

---
+++ combustivel/src/components/Config.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-layout>
+    <div slot="header" class="toolbar">
+      <q-toolbar-title :padding="0">
+        Configurações
+      </q-toolbar-title>
+    </div>
+
+
+    </div>
+  </q-layout>
+</template>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ combustivel/src/components/Resultado.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-layout>
+  <div slot="header" class="toolbar">
+  <q-toolbar-title :padding="0">
+  Resultado
+  </q-toolbar-title>
+  </div>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
